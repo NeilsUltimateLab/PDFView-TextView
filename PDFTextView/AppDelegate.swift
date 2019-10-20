eeb938b5f894aee6b70c8fb35479354ce451a135
NeilsUltimateLab <neilsultimatelab@icloud.com>
2019-10-20 19:55:55 +0530

    Initial Commit

---
+++ PDFTextView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PDFTextView
+//
+//  Created by Neil Jain on 10/20/19.
+//  Copyright © 2019 NeilsUltimateLab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PDFTextView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PDFTextView
+//
+//  Created by Neil Jain on 10/20/19.
+//  Copyright © 2019 NeilsUltimateLab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
